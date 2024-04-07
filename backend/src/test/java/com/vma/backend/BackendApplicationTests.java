b1d92adeab2e1527ffcdf7ddaeb3e5b97b251aff
davpeter <davidlaszlo.peter@blackrock.com>
2024-04-07 19:29:54 +0200

    Initialize Spring Boot backend

---
+++ backend/src/test/java/com/vma/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vma.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
