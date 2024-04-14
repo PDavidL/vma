f5903f1a2833443cb3b05e220cc165d823abae99
davpeter <davidlaszlo.peter@blackrock.com>
2024-04-14 12:55:47 +0200

    Add google map to the frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Vehicle Monitoring Application';
+
+  display: any;
+  center: google.maps.LatLngLiteral = {
+      lat: 47.47581,
+      lng: 19.05749
+  };
+  zoom = 15;
+
+  /*------------------------------------------
+  --------------------------------------------
+  moveMap()
+  --------------------------------------------
+  --------------------------------------------*/
+  moveMap(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  move()
+  --------------------------------------------
+  --------------------------------------------*/
+  move(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 }
